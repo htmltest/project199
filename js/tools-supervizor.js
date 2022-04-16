2c28f8509eda5b79f6704678497003abc34702fe
htmltest <struev.vs@gmail.com>
2022-04-16 23:16:20 +0300

    update
    report+analytics

--- js/tools-supervizor.js
+++ js/tools-supervizor.js
@@ -5,6 +5,54 @@ $(document).ready(function() {
         e.preventDefault();
     });
 
+    $('body').on('click', '.report-item-header-content a', function(e) {
+        var curItem = $(this).parents().filter('.report-item');
+        curItem.toggleClass('open');
+        curItem.find('.report-item-content').slideToggle();
+        e.preventDefault();
+    });
+
+    $('.supervizor-saloons-filter-link a').click(function(e) {
+        $('html').toggleClass('filter-open');
+        e.preventDefault();
+    });
+
+    $('.supervizor-saloons-filter-q-current').click(function() {
+        $(this).parent().toggleClass('open');
+    });
+
+    $(document).click(function(e) {
+        if ($(e.target).parents().filter('.supervizor-saloons-filter-q').length == 0) {
+            $('.supervizor-saloons-filter-q').removeClass('open');
+        }
+    });
+
+    $('.supervizor-saloons-filter-q-list-row-q input').change(function() {
+        updateSupervizorSaloonsFilterQ();
+    });
+
+    $('.supervizor-saloons-filter-q').each(function() {
+        updateSupervizorSaloonsFilterQ();
+    });
+
+    $('.supervizor-saloons-filter-status-current').click(function() {
+        $(this).parent().toggleClass('open');
+    });
+
+    $(document).click(function(e) {
+        if ($(e.target).parents().filter('.supervizor-saloons-filter-status').length == 0) {
+            $('.supervizor-saloons-filter-status').removeClass('open');
+        }
+    });
+
+    $('.supervizor-saloons-filter-status-list-row input').change(function() {
+        updateSupervizorSaloonsFilterStatus();
+    });
+
+    $('.supervizor-saloons-filter-status').each(function() {
+        updateSupervizorSaloonsFilterStatus();
+    });
+
 });
 
 $(window).on('load resize', function() {
@@ -34,3 +82,29 @@ $(window).on('load resize', function() {
     });
 
 });
+
+function updateSupervizorSaloonsFilterQ() {
+    var newText = '';
+
+    $('.supervizor-saloons-filter-q-list-row-q input:checked').each(function() {
+        if (newText != '') {
+            newText += '; ';
+        }
+        newText += $(this).parents().filter('.supervizor-saloons-filter-q-list-row').find('.supervizor-saloons-filter-q-list-row-y').html() + ', '+ $(this).parent().find('span').html();
+    });
+
+    $('.supervizor-saloons-filter-q-current span').html(newText);
+}
+
+function updateSupervizorSaloonsFilterStatus() {
+    var newText = '';
+
+    $('.supervizor-saloons-filter-status-list-row input:checked').each(function() {
+        if (newText != '') {
+            newText += ', ';
+        }
+        newText += $(this).parent().find('span em').html();
+    });
+
+    $('.supervizor-saloons-filter-status-current span').html(newText);
+}
